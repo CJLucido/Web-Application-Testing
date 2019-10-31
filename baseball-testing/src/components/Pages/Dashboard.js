cef9f42a942c5f061c9521040456ad67082c169d
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-31 13:54:29 -0700

    added basic counter functionality for Strike, ball, and foul buttons...need to add hit functionality

--- baseball-testing/src/components/Pages/Dashboard.js
+++ baseball-testing/src/components/Pages/Dashboard.js
@@ -4,20 +4,41 @@ import Display from "../Organisms/Display"
 export default function Dashboard (){
 
     const [numOfStrikes, setNumOfStrikes] = useState(0)
+    const [numOfBalls, setNumOfBalls] = useState(0)
 
-
-    const handleStrike = () => {
-
+     const handleStrike = () => {
+        if(numOfStrikes === 3){
+            setNumOfStrikes(0)
+        }else{
         setNumOfStrikes(numOfStrikes + 1)
-        console.log("this is number of strikes", numOfStrikes)
+        }
+     
     }
+
+    const handleFoul = () => {
+        if(numOfStrikes === 2){
+            setNumOfStrikes(numOfStrikes)
+        }else if (numOfStrikes <= 1) {
+            setNumOfStrikes(numOfStrikes + 1)
+        }
+    }
+
+    const handleBall = () => {
+        if(numOfBalls === 4){
+            setNumOfBalls(0)
+        }else {
+            setNumOfBalls(numOfBalls + 1)
+        }
+    }
+
     console.log("this is number of strikes", numOfStrikes)
+    console.log("this is number of balls", numOfBalls)
     return (
         <div>
-            <Display numOfStrikes={numOfStrikes} />
-            <button name='strikeBtn' onClick={handleStrike} data-testid="strikeBtn">Strike</button>
-            <button name='ballBtn' data-testid="ballBtn">Ball</button>
-            <button name='foulBtn' data-testid="foulBtn">Foul</button>
+            <Display numOfStrikes={numOfStrikes} numOfBalls={numOfBalls}/>
+            <button name='strikeBtn' onClick={()=>handleStrike()} data-testid="strikeBtn">Strike</button>
+            <button name='ballBtn' onClick={()=>handleBall()} data-testid="ballBtn">Ball</button>
+            <button name='foulBtn' onClick={() => handleFoul()} data-testid="foulBtn">Foul</button>
             <button name='hitBtn' data-testid="hitBtn">Hit</button>
         
         
--- baseball-testing/src/components/Pages/Dashboard.test.js
+++ baseball-testing/src/components/Pages/Dashboard.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {render, getByTestId} from "@testing-library/react"
 import App from "../../App"
+import Dashboard from "./Dashboard"
 
 
 it('renders 4 buttons', () => {
     const {getByTestId} = render(<App />)
-    getByTestId("strikeBtn")
+
+    getByTestId( "strikeBtn")
     getByTestId("ballBtn")
     getByTestId("foulBtn")
-    getByTestId("hitBtn")
+    getByTestId( "hitBtn")
+})
+
+it('increases non-visible strikes by 1', () => {
+    expect(handleStrike(numOfStrikes)).toBe(numOfStrikes +1)
 })
